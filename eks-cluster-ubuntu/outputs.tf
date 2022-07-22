a4f1672e89b93e2f7be76eba2516d09a25192841
MarinaCecan <100246922+MarinaCecan@users.noreply.github.com>
2022-07-22 17:06:39 -0500

    Create outputs.tf

---
+++ eks-cluster-ubuntu/outputs.tf
@@ -0,0 +1,8 @@
+output "cluster_endpoint" {
+  description = "Endpoint for EKS control plane"
+  value       = module.eks_cluster.endpoint
+}
+
+output "oidc" {
+  value = module.eks_cluster.oidc
+}
