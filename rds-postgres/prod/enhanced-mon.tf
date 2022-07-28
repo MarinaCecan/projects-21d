e95c282109a95a7a1c1c9440d3a0e3c662f10e89
MarinaCecan <100246922+MarinaCecan@users.noreply.github.com>
2022-07-28 13:20:16 -0500

    Create enhanced-mon.tf

---
+++ rds-postgres/prod/enhanced-mon.tf
@@ -0,0 +1,25 @@
+# Create an IAM role to allow enhanced monitoring
+resource "aws_iam_role" "rds_enhanced_monitoring" {
+  name_prefix        = var.name_prefix
+  assume_role_policy = data.aws_iam_policy_document.rds_enhanced_monitoring.json
+}
+
+resource "aws_iam_role_policy_attachment" "rds_enhanced_monitoring" {
+  role       = aws_iam_role.rds_enhanced_monitoring.name
+  policy_arn = var.policy_arn
+}
+
+data "aws_iam_policy_document" "rds_enhanced_monitoring" {
+  statement {
+    actions = [
+      "sts:AssumeRole",
+    ]
+
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["monitoring.rds.amazonaws.com"]
+    }
+  }
+}
